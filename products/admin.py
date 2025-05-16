9dc70567e426f5d1e9380ac736a426fd6e15772e
ZUB-20 <zubcictom@gmail.com>
2025-05-16 20:18:53 +0200

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import (Product,
+                     Ingredient)
+
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Product)
